b9e0612fb637d396e8ed4689c2a48e84ce995628
Duan Youhong <1149193481@qq.com>
2019-12-15 11:59:25 +0800

    first commit xiaoduan dev2.0

---
+++ test-order/src/main/java/com/atguigu/test/order/controller.java
@@ -0,0 +1,7 @@
+package com.atguigu.test.order;
+
+public class controller {
+    public static void main(String[] args) {
+        System.out.println("这是订单模块");
+    }
+}
---
+++ test-order/src/test/java/com/atguigu/test/order/TestOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.test.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
